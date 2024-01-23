24920f01c3238fb56fb3797de3d80a627d5b7e78
elidiojr <elidiomj@gmail.com>
2024-01-23 17:55:55 -0300

    entrega do projeto

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-console.log("oi");
+let nomeHeroi = "Elidio";
+let quantXP = 10001;
+let nivelHeroi = "";
+
+switch (true) {
+case (quantXP < 1001): 
+    nivelHeroi = "Ferro";
+    break; 
+case (quantXP > 1000 && quantXP < 2001):
+    nivelHeroi = "Bronze";
+    break;
+case (quantXP > 2000 && quantXP < 5001):
+    nivelHeroi = "Prata";
+    break;
+case (quantXP > 5000 && quantXP < 7001):
+    nivelHeroi = "Ouro";
+    break;
+case (quantXP > 7000 && quantXP < 8001):
+    nivelHeroi = "Platina";
+    break;      
+case (quantXP > 8000 && quantXP < 9001):
+    nivelHeroi = "Ascendente";
+    break;
+case (quantXP > 9000 && quantXP < 10001):
+    nivelHeroi = "Imortal";
+    break;
+default:
+    nivelHeroi = "Radiante";    
+}
+
+console.log("O Herói de nome **{ " + nomeHeroi + " }** está no nível de **{ "+ nivelHeroi + " }**");
+
